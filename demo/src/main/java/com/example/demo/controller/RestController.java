d26d504b12bce9f6dff443506c5597ee66c46d31
ravi991 <raviranjancs4@gmail.com>
2022-03-26 04:49:00 -0400

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/controller/RestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/rest")
+public class RestController {
+	
+	
+	@GetMapping
+	public String name() {
+		return "Welcome Gandu";
+		
+	}
+
+}
